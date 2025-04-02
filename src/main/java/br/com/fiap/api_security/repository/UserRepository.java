a26b2b0816f99454158d3e0a040dd7ba9f8f605e
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-04-01 22:52:56 -0300

    feat: cria UserRepository com busca por username para integração com Spring Security

--- src/main/java/br/com/fiap/api_security/repository/UserRepository.java
+++ src/main/java/br/com/fiap/api_security/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package br.com.fiap.api_security.repository;
 
-public interface UserRepository {
+import br.com.fiap.api_security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, UUID> {
+    UserDetails findByUsername(String username);
 }
