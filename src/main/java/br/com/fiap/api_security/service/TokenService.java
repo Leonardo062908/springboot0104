5e52270538f84fd45fb94b1fa6c3286fa253fb58
Leonardo062908 <leonardomatheus9155@gmail.com>
2025-04-15 22:48:27 -0300

    Feat: Garantindo a criação do token ao usuario

--- src/main/java/br/com/fiap/api_security/service/TokenService.java
+++ src/main/java/br/com/fiap/api_security/service/TokenService.java
@@ -1,5 +1,9 @@
 package br.com.fiap.api_security.service;
 
+import br.com.fiap.api_security.model.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +13,20 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    
+    public TokenService(String secret) {
+        this.secret = secret;
+    }
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("apisecurity")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(genExpirationInstant())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro na geração do token", exception);
+        }
+    }
 }
